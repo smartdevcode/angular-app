10568c570f0705370a2dbf9736e45f80812857c9
SeniorDevCode <senior.dev.software@gmail.com>
2012-09-28 20:17:20 +0100

    Initial clientside auth stuff

---
+++ src/app/admin/users/admin-users.js
@@ -0,0 +1,90 @@
+angular.module('admin-users', ['services.crud'], ['$routeProvider', 'routeCRUDProvider', function ($routeProvider, routeCRUDProvider) {
+
+  routeCRUDProvider.defineRoutes($routeProvider, '/admin/users', 'admin/users', 'Users', [], {
+    listItems:{'users': function(Users, $route){
+      return Users.all();
+    }},
+    newItem:{'user':function (Users, $route) {
+      return new Users();
+    }},
+    editItem:{'user':function (Users, $route) {
+      return Users.getById($route.current.params.itemId);
+    }}
+  });
+}]);
+
+angular.module('admin-users').controller('UsersListCtrl', ['$scope', '$location', 'users', function ($scope, $location, users) {
+  $scope.users = users;
+
+  $scope.itemView = function (item) {
+    $location.path('/admin/users/' + item.$id());
+  };
+}]);
+
+angular.module('admin-users').controller('UsersEditCtrl', ['$scope', '$location', 'crudMethods', 'user', function ($scope, $location, crudMethods, user) {
+
+  $scope.password = user.password;
+  angular.extend($scope, crudMethods('item', user, 'form', function () {
+    $location.path('/admin/users');
+  }, function() {
+    $scope.updateError = true;
+  }));
+}]);
+
+angular.module('admin-users').directive('uniqueEmail', ["Users", function (Users) {
+  return {
+    require:'ngModel',
+    restrict:'A',
+    link:function (scope, el, attrs, ctrl) {
+
+      //using push() here to run it as the last parser, after we are sure that other validators were run
+      ctrl.$parsers.push(function (viewValue) {
+
+        ctrl.$setValidity('uniqueEmail', false);
+        if (viewValue) {
+          Users.query({email:viewValue}, function (users) {
+            if (users.length === 0) {
+              ctrl.$setValidity('uniqueEmail', true);
+            }
+          });
+          return viewValue;
+        } else {
+          return undefined;
+        }
+      });
+    }
+  };
+}]);
+
+angular.module('admin-users').directive('validateEquals', function() {
+
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+
+      function validateEqual(myValue, otherValue) {
+        if (myValue === otherValue) {
+          ctrl.$setValidity('equal', true);
+          return myValue;
+        } else {
+          ctrl.$setValidity('equal', false);
+          return undefined;
+        }
+      }
+
+      scope.$watch(attrs.validateEquals, function(otherModelValue) {
+        console.log('xx');
+        ctrl.$setValidity('equal', ctrl.$viewValue === otherModelValue);
+      });
+
+      ctrl.$parsers.push(function(viewValue) {
+        return validateEqual(viewValue, scope.$eval(attrs.validateEquals));
+      });
+
+      ctrl.$formatters.push(function(modelValue) {
+        return validateEqual(modelValue, scope.$eval(attrs.validateEquals));
+      });
+    }
+  };
+});
---
+++ src/app/app.js
@@ -0,0 +1,43 @@
+angular.module('app', ['signin', 'dashboard', 'projects', 'admin', 'services.util', 'directives.crud', 'templates']);
+
+angular.module('app').constant('MONGOLAB_CONFIG', {
+//  baseUrl: 'https://api.mongolab.com/api/1/databases/',
+  baseUrl: 'http://localhost:3000/databases/',
+  dbName: 'ascrum'
+});
+
+angular.module('app').config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.otherwise({redirectTo:'/signin'});
+}]);
+
+angular.module('app').controller('AppCtrl', ['$scope', '$location', '$route', 'Security', 'HTTPRequestTracker', function ($scope, $location, $route, Security, HTTPRequestTracker) {
+  $scope.location = $location;
+  $scope.security = Security;
+
+  $scope.isNavbarActive = function (navBarPath) {
+    return navBarPath === $scope.pathElements[0];
+  };
+
+  $scope.$watch('location.path()', function (newValue) {
+    var pathElements = newValue.split('/');
+    pathElements.shift();
+    $scope.pathElements = pathElements;
+  });
+
+  $scope.breadcrumbPath = function (index) {
+    return '/' + ($scope.pathElements.slice(0, index + 1)).join('/');
+  };
+
+  $scope.hasPendingRequests = function () {
+    return HTTPRequestTracker.hasPendingRequests();
+  };
+
+  $scope.$on('$routeChangeError', function(event, current, previous, rejection){
+    //TODO: this is too MongoLab specific, itroduce error resolution service
+    $scope.routeChangeError = 'Route change error: '+rejection.code;
+  });
+
+  $scope.$on('$routeChangeStart', function(event, next, current){
+    $scope.routeChangeError = undefined;
+  });
+}]);
