17cf490cbc94d94111f883abf9107ea567846aec
SeniorDevCode <senior.dev.software@gmail.com>
2012-10-02 18:56:49 +0100

    Find by email now checks to see if a result even exists.

--- src/app/dashboard/dashboard.js
+++
@@ -1,9 +0,0 @@
-angular.module('dashboard',['services.projects'], ['$routeProvider', function($routeProvider){
-  $routeProvider.when('/dashboard', {
-    templateUrl:'dashboard/dashboard.tpl.html',
-    controller:'DashboardCtrl'
-  });
-}]);
-
-angular.module('dashboard').controller('DashboardCtrl', ['$scope', function($scope){
-}]);
